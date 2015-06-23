b30501cf4eddb82528f61dcfd07f019eb7e6871a
hsw <hsws83@naver.com>
2015-06-23 05:12:36 +0000

    실제 이메일 전송 기능 탑재하기

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -3,7 +3,19 @@ class EmailController < ApplicationController
 	end
 
 	def email_send
-		@email = params[:email]
-		@content = params[:content]
+		email = params[:email]
+		title = params[:title]
+		content = params[:content]
+
+		gmail = Gmail.connect("hsw1240@hermesys.co.kr","dnrl-1240")
+		gmail.deliver do
+			to email
+			subject title
+			text_part do
+				body content
+			end
+		end
+	
+	gmail.logout
 	end
 end
